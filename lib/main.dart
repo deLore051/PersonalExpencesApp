5ac424acb1e464ec3e397dccd8fbfaa2df5067ed
deLore051 <stefan.dojcinovic051@gmail.com>
2022-07-26 09:29:47 +0200

    Worked on the Expences tile (Container, Row, Column) and style for Text

--- lib/main.dart
+++ lib/main.dart
@@ -65,14 +65,46 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Container(
-                        child: Text(
-                          tx.amount.toString()
+                        padding: EdgeInsets.all(10),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10, 
+                          horizontal: 15
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
                           ),
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                            style: BorderStyle.solid
+                          ),
+                        ),
+                        child: Text(
+                          "\$${tx.amount}",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(tx.title),
-                          Text(tx.date.toString()),
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                              color: Colors.black54
+                            ),
+                          ),
                         ],
                       )
                     ],
