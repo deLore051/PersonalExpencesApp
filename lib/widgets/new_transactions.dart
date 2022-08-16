35304444b948d7fadfbb4dbbb1a53ebc686686db
deLore051 <stefan.dojcinovic051@gmail.com>
2022-08-16 11:36:54 +0200

    Moved our code for chose date text button to new adaptive button widget

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:expences_app/widgets/adaptive_flat_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -70,76 +71,46 @@ class _NewTransactionState extends State<NewTransaction> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.only(
-                  left: 10, 
-                  top: 10, 
-                  right: 10, 
-                  bottom: MediaQuery.of(context).viewInsets.bottom + 10
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Title"
-                      ),
-                      controller: _titleController,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Amount"
-                      ),
-                      keyboardType: TextInputType.number,
-                      controller: _amountController,
-                      /* The on submitted takes a Function with a string value as a paameter so
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+              left: 10,
+              top: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: "Title"),
+                controller: _titleController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Amount"),
+                keyboardType: TextInputType.number,
+                controller: _amountController,
+                /* The on submitted takes a Function with a string value as a paameter so
                         we eather have to add a String parameter in submitData or in our case
                         make an annonymous function and put an _ as a parameter because we wont
                         use this parameter someware else, so we dont care how its called. */
-                      onSubmitted: (_) => _submitData(),
-                    ),
-                    Container(
-                      height: 70,
-                      width: double.infinity,
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Text(
-                            _selectedDate == null 
-                            ? "No date chosen!" 
+                onSubmitted: (_) => _submitData(),
+              ),
+              Container(
+                height: 70,
+                width: double.infinity,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? "No date chosen!"
                             : "Picked date: ${DateFormat("dd/MM/yyyy").format(_selectedDate!)}",
-                            style: TextStyle(
-                              fontSize: 16,
-                            ),
-                          ),
-                          ),
-                          Platform.isIOS
-                        ? CupertinoButton(
-                          child: Text(
-                              "Chose date",
-                              style: TextStyle(
-                                  fontSize: 16, 
-                                  fontWeight: FontWeight.bold
-                                ),
-                            ),
-                          onPressed: _presentDatePicker,
-                        )
-                        : TextButton(
-                            style: ButtonStyle(
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                Theme.of(context).primaryColor,
-                              ),
-                            ),
-                            onPressed: _presentDatePicker,
-                            child: Text(
-                              "Chose date",
-                              style: TextStyle(
-                                  fontSize: 16, 
-                                  fontWeight: FontWeight.bold
-                                ),
-                            ),
-                          ),
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    AdaptiveFlatButton("Chose Date", _presentDatePicker),
                   ],
                 ),
               ),
